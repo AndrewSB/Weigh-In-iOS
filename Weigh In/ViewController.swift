56aa8e6b5cd93a51ce28e8457d2627415f3804c2
AndrewSB <asbreckenridge@me.com>
2014-12-07 14:42:11 -0600

    added new proj

---
+++ Weigh In/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weigh In
+//
+//  Created by Andrew Breckenridge on 12/7/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
